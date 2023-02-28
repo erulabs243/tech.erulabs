d6f7a6b512c5715083430ab044a6f22d5784eff8
EruLabs <dev.erulabs@gmail.com>
2023-02-28 16:00:19 +0200

    Default homepage layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {},
+    extend: {
+      container: {
+        center: true,
+      },
+    },
   },
   daisyui: {
     themes: [
